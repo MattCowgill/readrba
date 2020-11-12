1ffc70758a2392b1b5dea19c2779c0e7fdb7af2b
Matt Cowgill <mattcowgill@gmail.com>
2020-11-13 09:32:21 +1100

    ensure tests expect character

--- tests/testthat/test-tables_from_seriesid.R
+++ tests/testthat/test-tables_from_seriesid.R
@@ -8,7 +8,7 @@ test_that("correct tableis identified when single series_id supplied", {
     length(single_id$table_no),
     length(single_id$cur_hist)
   )
-  expect_equal(single_id$table_no[1], "G1")
+  expect_equal(single_id$table_no[1], as.character("G1"))
 })
 
 test_that("correct table is identified when multiple series_ids from one table are supplied", {
@@ -22,7 +22,7 @@ test_that("correct table is identified when multiple series_ids from one table a
     length(multiple_ids$table_no),
     length(multiple_ids$cur_hist)
   )
-  expect_equal(multiple_ids$table_no[1], "G1")
+  expect_equal(multiple_ids$table_no[1], as.character("G1"))
 })
 
 test_that("correct tables are identified when multiple series_ids from different tables are supplied", {
@@ -36,7 +36,7 @@ test_that("correct tables are identified when multiple series_ids from different
     length(multiple_ids$table_no),
     length(multiple_ids$cur_hist)
   )
-  expect_equal(multiple_ids$table_no, c("A1", "A1.1", "G1"))
+  expect_equal(multiple_ids$table_no, as.character(c("A1", "A1.1", "G1")))
 })
 
 test_that("tables_from_series_id() fails when incorrect series_id(s) supplied", {
