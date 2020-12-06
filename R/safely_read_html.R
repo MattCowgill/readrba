5fd762bc138ce280200df8ac452e3cef931e8070
Matt Cowgill <mattcowgill@gmail.com>
2020-12-06 15:04:32 +0800

    try again if scraping fails

--- R/safely_read_html.R
+++ R/safely_read_html.R
@@ -1,4 +1,8 @@
-#' `read_html()` safely
+#' `read_html()` safely.
+#'
+#' The purpose of this function is to specify a user agent for scraping,
+#' and to return a useful error if the page cannot be scraped.
+#'
 #' @keywords internal
 #' @param url url to read with `xml2::read_html()`
 #' @param ... arguments passed to `xml2::read_html()`
@@ -9,14 +13,23 @@ safely_read_html <- function(url, ...) {
 
   do_safely_read_html <- purrr::safely(xml2::read_html)
 
-  x <- do_safely_read_html(url,
+  x <- do_safely_read_html(x = url,
                            ...,
                            user_agent = "readrba R package - https://mattcowgill.github.io/readrba/index.html")
 
 
-  if (is.null(x$error)) {
-    return(x$result)
-  } else {
-    stop("Could not read HTML at ", url)
+  if (!is.null(x$error)) {
+    # Try again with a delay
+    Sys.sleep(5)
+    x <- do_safely_read_html(url,
+                             ...,
+                             user_agent = "readrba R package - https://mattcowgill.github.io/readrba/index.html")
   }
+
+  if (!is.null(x$error)) {
+    stop("Could not read HTML at ", url)
+  } else {
+    return(x$result)
+  }
+
 }
