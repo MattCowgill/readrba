c9358b157b2cd2e36fb7daeadb4d52469edf6aa9
Matt Cowgill <mattcowgill@gmail.com>
2020-12-06 13:18:39 +0800

    speed up url checking

--- R/url_exists.R
+++ R/url_exists.R
@@ -1,9 +1,24 @@
 #' Check if URLs are valid
 #' @param urls Character vector containing url(s)
 #' @return Logical vector; names of the vector are the supplied urls
-#' @noRd
+#' @keywords internal
+#' @rdname url_exists
 url_exists <- function(urls) {
-  x <- purrr::map_lgl(urls, ~ !httr::http_error(.x))
-  x <- rlang::set_names(x, urls)
+  x <- purrr::map_lgl(urls, check_url_success)
   x
 }
+
+#' Check a single URL's status
+#' Internal function used by `url_exists()`
+#' @return Logical. If status category = "Success", `TRUE`, else `FALSE`.
+#' @param url Character vector of length 1; a url incl. http/https, such as
+#' `https://www.google.com`.
+#' @keywords internal
+#' @rdname url_exists
+check_url_success <- function(url) {
+  x <- httr::GET(url)
+  x <- httr::http_status(x)
+  result <- ifelse(x[["category"]] == "Success", TRUE, FALSE)
+  result
+}
+
