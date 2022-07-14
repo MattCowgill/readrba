90077d681ac7ec38705880e61ea545d64471fb28
“Matt <mattcowgill@gmail.com>
2022-07-14 14:55:51 +1000

    expect fewer rows in forecasts

--- tests/testthat/test-rba_forecasts.R
+++ tests/testthat/test-rba_forecasts.R
@@ -2,7 +2,7 @@ test_that("rba_forecasts() returns expected output from internal data", {
   offline_forecasts <- rba_forecasts(refresh = FALSE)
 
   expect_is(offline_forecasts, "tbl_df")
-  expect_gt(nrow(offline_forecasts), 6000)
+  expect_gt(nrow(offline_forecasts), 5000)
   expect_false(any(is.na(offline_forecasts$value)))
   expect_length(offline_forecasts, 8)
   expect_equal(min(offline_forecasts$forecast_date), as.Date("1990-03-01"))
@@ -35,7 +35,7 @@ test_that("rba_forecasts() returns expected output when refreshed", {
   )
 
   expect_is(refreshed_forecasts, "tbl_df")
-  expect_gt(nrow(refreshed_forecasts), 6000)
+  expect_gt(nrow(refreshed_forecasts), 5000)
   expect_false(any(is.na(refreshed_forecasts$value)))
   expect_length(refreshed_forecasts, 8)
   expect_equal(min(refreshed_forecasts$forecast_date), as.Date("1990-03-01"))
