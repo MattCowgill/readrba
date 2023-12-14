d542e7664f58ec88b0e0c7fe645e427327008e48
Matt Cowgill <mattcowgill@gmail.com>
2023-12-15 09:42:01 +1100

    tweak internal forecasts

--- data-raw/internal_data.R
+++ data-raw/internal_data.R
@@ -55,82 +55,88 @@ series_list <- all_data %>%
 # As compiled in Tulip and Wallace RDP2012-07
 # https://www.rba.gov.au/statistics/historical-forecasts.html
 
-hist_forecast_url <- "https://www.rba.gov.au/statistics/xls/forecast-date-by-event-date.xls"
-hist_forecast_file <- tempfile(fileext = ".xls")
-download.file(hist_forecast_url, hist_forecast_file)
-hist_sheets <- excel_sheets(hist_forecast_file)[excel_sheets(hist_forecast_file) != "Notes"]
+hist_forecasts_rda <- file.path("data-raw", "hist_forecasts.Rda")
 
-load_hist_sheet <- function(filename, sheet_name) {
-  raw_sheet <- read_excel_noguess(
-    path = filename,
-    sheet = sheet_name,
-    col_names = FALSE,
-    .name_repair = "minimal"
+if (!file.exists(hist_forecasts_rda)) {
+  hist_forecast_url <- "https://www.rba.gov.au/statistics/xls/forecast-date-by-event-date.xls"
+  hist_forecast_file <- tempfile(fileext = ".xls")
+  download.file(hist_forecast_url, hist_forecast_file)
+  hist_sheets <- excel_sheets(hist_forecast_file)[excel_sheets(hist_forecast_file) != "Notes"]
+
+  load_hist_sheet <- function(filename, sheet_name) {
+    raw_sheet <- read_excel_noguess(
+      path = filename,
+      sheet = sheet_name,
+      col_names = FALSE,
+      .name_repair = "minimal"
+    )
+
+    n_col <- ncol(raw_sheet)
+    forecast_date <- as.character(raw_sheet[1, 2:n_col])
+    notes <- as.character(raw_sheet[2, 2:n_col])
+    source <- as.character(raw_sheet[3, 2:n_col])
+
+    series <- paste(forecast_date, notes, source, sep = ";")
+
+    data <- raw_sheet[4:nrow(raw_sheet), ]
+    names(data) <- c("date", series)
+
+    data <- data %>%
+      tidyr::gather(
+        key = series,
+        value = value,
+        -date
+      ) %>%
+      tidyr::separate(series,
+                      into = c("forecast_date", "notes", "source"),
+                      sep = ";"
+      ) %>%
+      mutate(across(everything(), \(x) na_if(x, y = "NA")))
+
+    data <- data %>%
+      separate(date, into = c("year", "quarter"), sep = "Q") %>%
+      mutate(across(c(year, quarter), as.numeric)) %>%
+      mutate(
+        month = (quarter * 3),
+        date = lubridate::dmy(paste("01", month, year, sep = "-")),
+        year_qtr = lubridate::quarter(date, with_year = TRUE)
+      ) %>%
+      select(-year, -quarter, -month) %>%
+      mutate(forecast_date = janitor::excel_numeric_to_date(as.numeric(forecast_date)))
+
+    data
+  }
+
+
+  hist_forecasts <- purrr::map_dfr(
+    .x = purrr::set_names(hist_sheets, hist_sheets),
+    .f = load_hist_sheet,
+    filename = hist_forecast_file,
+    .id = "series_desc"
   )
 
-  n_col <- ncol(raw_sheet)
-  forecast_date <- as.character(raw_sheet[1, 2:n_col])
-  notes <- as.character(raw_sheet[2, 2:n_col])
-  source <- as.character(raw_sheet[3, 2:n_col])
+  hist_forecasts <- hist_forecasts %>%
+    mutate(series = case_when(
+      series_desc == "GDP - 1 quarter change" ~ "gdp_change",
+      series_desc == "GDP - Level" ~ "gdp_level",
+      series_desc == "CPI - 4 quarter change" ~ "cpi_annual_inflation",
+      series_desc == "Underlying - 4 quarter change" ~ "underlying_annual_inflation",
+      series_desc == "Underlying - 1 quarter change" ~ "underlying_quarterly_inflation",
+      series_desc == "Unemployment rate - Level" ~ "unemp_rate",
+      TRUE ~ NA_character_
+    ))
 
-  series <- paste(forecast_date, notes, source, sep = ";")
+  # Expect lots of NAs introduced by coercion here
+  hist_forecasts$value <- as.numeric(hist_forecasts$value)
 
-  data <- raw_sheet[4:nrow(raw_sheet), ]
-  names(data) <- c("date", series)
+  hist_forecasts <- hist_forecasts %>%
+    filter(!is.na(value))
 
-  data <- data %>%
-    tidyr::gather(
-      key = series,
-      value = value,
-      -date
-    ) %>%
-    tidyr::separate(series,
-      into = c("forecast_date", "notes", "source"),
-      sep = ";"
-    ) %>%
-    mutate(across(everything(), \(x) na_if(x, y = "NA")))
-
-  data <- data %>%
-    separate(date, into = c("year", "quarter"), sep = "Q") %>%
-    mutate(across(c(year, quarter), as.numeric)) %>%
-    mutate(
-      month = (quarter * 3),
-      date = lubridate::dmy(paste("01", month, year, sep = "-")),
-      year_qtr = lubridate::quarter(date, with_year = TRUE)
-    ) %>%
-    select(-year, -quarter, -month) %>%
-    mutate(forecast_date = janitor::excel_numeric_to_date(as.numeric(forecast_date)))
-
-  data
+  save(hist_forecasts,
+       file = file.path("data-raw", "hist_forecasts.Rda"))
 }
 
-
-hist_forecasts <- purrr::map_dfr(
-  .x = purrr::set_names(hist_sheets, hist_sheets),
-  .f = load_hist_sheet,
-  filename = hist_forecast_file,
-  .id = "series_desc"
-)
-
-hist_forecasts <- hist_forecasts %>%
-  mutate(series = case_when(
-    series_desc == "GDP - 1 quarter change" ~ "gdp_change",
-    series_desc == "GDP - Level" ~ "gdp_level",
-    series_desc == "CPI - 4 quarter change" ~ "cpi_annual_inflation",
-    series_desc == "Underlying - 4 quarter change" ~ "underlying_annual_inflation",
-    series_desc == "Underlying - 1 quarter change" ~ "underlying_quarterly_inflation",
-    series_desc == "Unemployment rate - Level" ~ "unemp_rate",
-    TRUE ~ NA_character_
-  ))
-
-# Expect lots of NAs introduced by coercion here
-hist_forecasts$value <- as.numeric(hist_forecasts$value)
-
-hist_forecasts <- hist_forecasts %>%
-  filter(!is.na(value))
-
-save(hist_forecasts,
-     file = file.path("data-raw", "hist_forecasts.Rda"))
+load(hist_forecasts_rda)
 
 # Create 2014-18 forecasts -----
 # To 2014, we use the historical series published by Bishop & Tulip (above)
